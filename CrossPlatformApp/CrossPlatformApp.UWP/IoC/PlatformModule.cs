21dd965684953dff52cb6c825671ca515c4a0c8c
Andrey Zakharov <mediaexplorer74@hotmail.com>
2021-09-28 21:31:36 +0300

    .

---
+++ CrossPlatformApp/CrossPlatformApp.UWP/IoC/PlatformModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autofac;
+using CrossPlatformApp.Models;
+using CrossPlatformApp.UWP.Models;
+
+namespace CrossPlatformApp.UWP.IoC
+{
+    class PlatformModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<ClickModel>().As<IClickModel>();
+        }
+    }
+}
---
+++ CrossPlatformApp/CrossPlatformApp.UWP/Models/ClickModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CrossPlatformApp.Models;
+
+namespace CrossPlatformApp.UWP.Models
+{
+    class ClickModel : IClickModel
+    {
+        public int Clicks { get; private set; }
+
+        public void OnClick()
+        {
+            Clicks++;
+        }
+
+        public void Reset()
+        {
+            Clicks = 0;
+        }
+    }
+}
